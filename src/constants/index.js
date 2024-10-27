463318ba97f16860dfd749ba13b7eed576dcb53e
AReum <alliejj99@gmail.com>
2024-10-27 16:06:56 +0900

    푸터 영역

--- src/constants/index.js
+++ src/constants/index.js
@@ -207,38 +207,28 @@ export const contactText = [
 
 export const footerText = [
   {
-    title: "youtube",
-    desc: "유튜브에 오시면 더 많은 강의를 볼 수 있습니다.",
-    link: "https://www.youtube.com/@Webstoryboy",
+    title: "notion",
+    desc: "UI/UX 개발자, 장아름은 어떤 사람인가 더 알 수 있습니다.",
+    link: "https://languid-argon-8f2.notion.site/UI-UX-a02309618af04592a862aa2467c95722?pvs=4",
   },
   {
-    title: "github",
-    desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
-    link: "https://github.com/webstoryboy",
+    title: "github__01",
+    desc: "프론트엔드와 퍼블리셔를 같이 작업중인 소스들을 볼 수 있습니다.",
+    link: "https://github.com/alliejj99",
+  },
+  {
+    title: "github__02",
+    desc: "퍼블리셔를 준비할때 참고할 수 있는 다양한 소스들을 볼 수 있습니다.",
+    link: "https://github.com/alliejj99",
+  },
+  {
+    title: "CodePen",
+    desc: "다양한 효과들및 작업중인 이벤트등을 확인할 수 있습니다.",
+    link: "https://codepen.io/jangar6",
   },
   {
     title: "blog",
-    desc: "블러그에 오시면 더 많은 정보를 볼 수 있습니다.",
-    link: "https://webstoryboy.co.kr",
-  },
-  {
-    title: "gsap",
-    desc: "GSAP에 오시면 더 많은 강의를 볼 수 있습니다.",
-    link: "https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq",
-  },
-  {
-    title: "react",
-    desc: "리액트로 만든 사이트를 같이 만들어 봅니다.",
-    link: "https://github.com/webstoryboy/port2023-react",
-  },
-  {
-    title: "vue",
-    desc: "뷰로 만든 사이트를 같이 만들어 봅니다.",
-    link: "https://github.com/webstoryboy/port2023-vue",
-  },
-  {
-    title: "next",
-    desc: "넥스트로 만든 사이트를 같이 만들어 봅니다.",
-    link: "https://github.com/webstoryboy/port2023-next",
+    desc: "기본적인 스킬및 작업들을 정리한것을 확인 할 수 있습니다.",
+    link: "https://jangar6.github.io/dothome1/",
   },
 ];
