c99d6f73aefa82b143f3b3f19df142c015625a06
AReum <alliejj99@gmail.com>
2024-10-27 14:00:54 +0900

    오류 해결: css 파일 scss 로 확장자 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx');
+    return config;
+  },
+};
 
 export default nextConfig;
